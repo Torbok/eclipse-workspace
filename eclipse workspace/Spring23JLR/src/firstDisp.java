eb34341d764b3a00a535182174fa4c2d31ddd8fb
Torbok <squidarmada@gmail.com>
2023-01-26 11:34:24 -0600

    w

---
+++ eclipse workspace/Spring23JLR/src/firstDisp.java
@@ -0,0 +1,17 @@
+/* basic layout to variables
+*/
+public class firstDisp
+{
+   public static void main(String[] args){ 
+	  int numOne, answer;
+	  int numTwo = 0;
+	  double average;
+	  numOne = 23;
+	  numTwo = 10; 
+	  answer = numOne + numTwo;
+	  average=answer/2.0;
+        System.out.println("23+10= " + answer);
+        System.out.print(average);
+//      System.out.print("this is a good answer");
+   }
+}
