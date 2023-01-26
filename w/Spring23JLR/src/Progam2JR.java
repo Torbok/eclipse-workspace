b20306dc947d601490887d9153fdd9fc9f8eeac3
jrichm00 <jrichm00@BD14404.student.ad.uafortsmith.edu>
2023-01-26 16:26:37 -0600

    added more bills to Program2JR.java

--- w/Spring23JLR/src/Progam2JR.java
+++ w/Spring23JLR/src/Progam2JR.java
@@ -5,10 +5,14 @@ public class Progam2JR {
 		//1/23/2023
 		//Enter a dollar amount and determine least amount of bills to pay with
 		Scanner kb = new Scanner(System.in);
-		int twenties,tens,fives,ones,amount;
+		int hundreds,fifties,twenties,tens,fives,ones,amount;
 		System.out.print("Enter a dollar amount: ");
 		amount=kb.nextInt();
 		kb.nextLine();
+		hundreds=(amount-(amount%100))/100;
+		amount=amount-(hundreds*100);
+		fifties=(amount-(amount%50))/50;
+		amount=amount-(fifties*50);
 		twenties=(amount-(amount%20))/20;
 		amount=amount-(twenties*20);
 		tens=(amount-(amount%10))/10;
@@ -18,10 +22,13 @@ public class Progam2JR {
 		ones=amount;
 		//System.out.print(twenties+" "+tens+" "+fives+" "+ones);
 		//Used to test
+		System.out.println("$100 bills: "+hundreds);
+		System.out.println("$50 bills: "+fifties);
 		System.out.println("$20 bills: "+twenties);
 		System.out.println("$10 bills: "+tens);
 		System.out.println("$5 bills: "+fives);
 		System.out.println("$1 bills: "+ones);
+		System.out.println("Total Bills: "+(hundreds+fifties+twenties+tens+fives+ones));
 	}
 
 }
